52c81ab2fa6dca4cebf28b81a99f341ab72f38b2
jabelop <jabelop@hotmail.com>
2022-12-05 15:07:27 -0500

    - users module and users service added to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersServiceInterface } from './users/interfaces/users-service.interface';
+import { UsersModule } from './users/users.module';
+import { UsersService } from './users/users.service';
+
+const usersServiceProvider = {provide: UsersServiceInterface, useClass: UsersService};
 
 @Module({
-  imports: [],
+  imports: [UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    usersServiceProvider
+  ],
 })
 export class AppModule {}
