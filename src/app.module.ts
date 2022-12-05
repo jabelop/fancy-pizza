bca930546982b731bd00f11930b6036c852df112
jabelop <jabelop@hotmail.com>
2022-12-05 16:03:53 -0500

    - auth module, auth controller, auth service provider and jwt module added to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,25 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
+import { AuthService } from './auth/auth.service';
+import { AuthServiceInterface } from './auth/interfaces/auth-service.interface';
 import { UsersServiceInterface } from './users/interfaces/users-service.interface';
 import { UsersModule } from './users/users.module';
 import { UsersService } from './users/users.service';
 
 const usersServiceProvider = {provide: UsersServiceInterface, useClass: UsersService};
+const authServiceProvider = {provide: AuthServiceInterface, useClass: AuthService};
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
+  imports: [UsersModule, AuthModule, JwtModule],
+  controllers: [AppController, AuthController],
   providers: [
     AppService,
-    usersServiceProvider
+    usersServiceProvider,
+    authServiceProvider
   ],
 })
 export class AppModule {}
