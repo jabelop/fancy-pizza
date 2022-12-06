93c0ec7472e8589264f84027b077b09449ed45a4
jabelop <jabelop@hotmail.com>
2022-12-06 13:31:00 -0500

    - price property added to the topping interface

--- src/toppings/toppings.service.ts
+++ src/toppings/toppings.service.ts
@@ -3,88 +3,108 @@ import { Topping } from './interfaces/topping.interface';
 import { ToppingsServiceInterface } from './interfaces/toppings-service.interface';
 
 @Injectable()
-export class ToppingsService implements ToppingsServiceInterface{
+export class ToppingsService implements ToppingsServiceInterface {
     private readonly toppings: Topping[] = [
         {
             id: "1",
             name: 'Pineapple',
+            price: 2
         },
         {
             id: "2",
             name: 'Feta-type Cheese',
+            price: 2
         },
         {
             id: "3",
             name: 'Cheddar Cheese',
+            price: 2
         },
         {
             id: "4",
             name: 'Parmesan Type Cheese',
+            price: 2
         },
         {
             id: "5",
             name: 'Sweet Corn',
+            price: 2
         },
         {
             id: "6",
             name: 'Jalapeño Peppers',
+            price: 2
         },
         {
             id: "7",
             name: 'Fresh Mushrooms',
+            price: 3
         },
         {
             id: "8",
             name: 'Fresh Peppers',
+            price: 3
         },
         {
             id: "9",
             name: 'Fresh Tomato',
+            price: 3
         },
         {
             id: "10",
             name: 'Black Olives',
+            price: 3
         },
         {
             id: "11",
             name: 'Caramelized Onion',
+            price: 3
         },
         {
             id: "12",
             name: 'Purple Onion',
+            price: 3
         },
         {
             id: "13",
             name: 'Crispy Onion',
+            price: 3
         },
         {
             id: "14",
             name: 'Beef',
+            price: 4
         },
         {
             id: "15",
             name: 'Grilled Chicken',
+            price: 4
         },
         {
             id: "16",
             name: 'Ham',
+            price: 4
         },
         {
             id: "17",
             name: 'Bacon',
+            price: 4
         },
         {
             id: "18",
             name: 'Pepperoni',
+            price: 4
         },
         {
             id: "19",
             name: 'Tuna',
+            price: 4
         },
         {
             id: "20",
             name: 'Chicken with spices',
-        },
+            price: 4
+        }
     ];
 
     async findOne(id: string): Promise<Topping | undefined> {
